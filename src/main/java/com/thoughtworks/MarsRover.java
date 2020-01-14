8a85480e3a92e0748ee52d6f9b24c01c38035b22
luaifei <lu.aifei@thoughtworks.com>
2020-01-14 21:59:51 +0800

    实现火星车功能，集中到一个类中

--- src/main/java/com/thoughtworks/MarsRover.java
+++ src/main/java/com/thoughtworks/MarsRover.java
@@ -1,9 +1,83 @@
 package com.thoughtworks;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
 
 @AllArgsConstructor
 public class MarsRover {
     private RoverState state;
 
+    public RoverState execute(List<Cmd> cmdList) {
+        cmdList.forEach(cmd -> state = cmd.execute(state));
+        return state;
+    }
+}
+
+@AllArgsConstructor
+@EqualsAndHashCode
+class RoverState {
+    private int x;
+    private int y;
+    private Direction direction;
+
+    public RoverState move() {
+        int newX = x;
+        int newY = y;
+
+        switch (direction) {
+            case E: newX += 1; break;
+            case N: newY += 1; break;
+            case S: newY -= 1; break;
+            case W: newX -= 1; break;
+        }
+        return new RoverState(newX, newY, direction);
+    }
+
+    public RoverState turnRight() {
+        return new RoverState(x, y, direction.turnRight());
+    }
+
+    public RoverState turnLeft() {
+        return new RoverState(x, y, direction.turnLeft());
+    }
+}
+
+enum Direction {
+    N, E, S, W;
+
+    public Direction turnRight() {
+        return Direction.values()[(this.ordinal() + 1) % 4];
+    }
+
+    public Direction turnLeft() {
+        return Direction.values()[(this.ordinal() + 3) % 4];
+    }
+}
+
+interface Cmd {
+    RoverState execute(RoverState roverState);
+}
+
+class MoveCmd implements Cmd {
+
+    @Override
+    public RoverState execute(RoverState roverState) {
+        return roverState.move();
+    }
+}
+
+class TurnLeftCmd implements Cmd {
+    @Override
+    public RoverState execute(RoverState roverState) {
+        return roverState.turnLeft();
+    }
+}
+
+class TurnRightCmd implements Cmd {
+    @Override
+    public RoverState execute(RoverState roverState) {
+        return roverState.turnRight();
+    }
 }
--- src/test/java/com/thoughtworks/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/MarsRoverTest.java
@@ -2,15 +2,141 @@ package com.thoughtworks;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MarsRoverTest {
     @Test
     void should_create_mars_rover_given_rover_state() {
         RoverState roverState = new RoverState(0, 0, Direction.E);
-
         MarsRover marsRover = new MarsRover(roverState);
-
         assertNotNull(marsRover);
+    }
+
+    @Test
+    void should_return_E_when_turn_right_given_direction_is_N() {
+        Direction start = Direction.N;
+        Direction end = start.turnRight();
+        assertEquals(Direction.E, end);
+    }
+
+    @Test
+    void should_return_S_when_turn_right_given_direction_is_E() {
+        Direction start = Direction.E;
+        Direction end = start.turnRight();
+        assertEquals(Direction.S, end);
+    }
+
+    @Test
+    void should_return_W_when_turn_right_given_direction_is_S() {
+        Direction start = Direction.S;
+        Direction end = start.turnRight();
+        assertEquals(Direction.W, end);
+    }
+
+    @Test
+    void should_return_N_when_turn_right_given_direction_is_W() {
+        Direction start = Direction.W;
+        Direction end = start.turnRight();
+        assertEquals(Direction.N, end);
+    }
+
+    @Test
+    void should_return_W_when_turn_left_given_direction_is_N() {
+        Direction start = Direction.N;
+        Direction end = start.turnLeft();
+        assertEquals(Direction.W, end);
+    }
+
+    @Test
+    void should_return_S_when_turn_left_given_direction_is_W() {
+        Direction start = Direction.W;
+        Direction end = start.turnLeft();
+        assertEquals(Direction.S, end);
+    }
+
+    @Test
+    void should_return_E_when_turn_left_given_direction_is_S() {
+        Direction start = Direction.S;
+        Direction end = start.turnLeft();
+        assertEquals(Direction.E, end);
+    }
+
+    @Test
+    void should_return_N_when_turn_left_given_direction_is_E() {
+        Direction start = Direction.E;
+        Direction end = start.turnLeft();
+        assertEquals(Direction.N, end);
+    }
+
+    @Test
+    void should_x_increase_1_when_move_given_direction_is_E() {
+        RoverState roverState = new RoverState(0, 0, Direction.E);
+        RoverState newRoverState = roverState.move();
+        assertEquals(new RoverState(1, 0, Direction.E), newRoverState);
+    }
+
+    @Test
+    void should_y_increase_1_when_move_given_direction_is_N() {
+        RoverState roverState = new RoverState(0, 0, Direction.N);
+        RoverState newRoverState = roverState.move();
+        assertEquals(new RoverState(0, 1, Direction.N), newRoverState);
+    }
+
+    @Test
+    void should_y_decrease_1_when_move_given_direction_is_S() {
+        RoverState roverState = new RoverState(0, 0, Direction.S);
+        RoverState newRoverState = roverState.move();
+        assertEquals(new RoverState(0, -1, Direction.S), newRoverState);
+    }
+
+    @Test
+    void should_x_decrease_1_when_move_given_direction_is_W() {
+        RoverState roverState = new RoverState(0, 0, Direction.W);
+        RoverState newRoverState = roverState.move();
+        assertEquals(new RoverState(-1, 0, Direction.W), newRoverState);
+    }
+
+    @Test
+    void should_x_increase_when_mars_rover_execute_move_cmd_given_direction_is_E() {
+        RoverState start = new RoverState(0, 0, Direction.E);
+        MarsRover marsRover = new MarsRover(start);
+        RoverState end = marsRover.execute(Collections.singletonList(new MoveCmd()));
+        assertEquals(new RoverState(1, 0, Direction.E), end);
+    }
+
+    @Test
+    void should_direction_is_E_when_mars_rover_execute_turn_right_cmd_given_direction_is_N() {
+        RoverState start = new RoverState(0, 0, Direction.N);
+        MarsRover marsRover = new MarsRover(start);
+        RoverState end = marsRover.execute(Collections.singletonList(new TurnRightCmd()));
+        assertEquals(new RoverState(0, 0, Direction.E), end);
+    }
+
+    @Test
+    void should_direction_is_W_when_mars_rover_execute_turn_left_cmd_given_direction_is_N() {
+        RoverState start = new RoverState(0, 0, Direction.N);
+        MarsRover marsRover = new MarsRover(start);
+        RoverState end = marsRover.execute(Collections.singletonList(new TurnLeftCmd()));
+        assertEquals(new RoverState(0, 0, Direction.W), end);
+    }
+
+    @Test
+    void should_direction_is_W_and_x_decrease_1_when_mars_rover_execute_turn_left_and_move_cmd_given_direction_is_N() {
+        RoverState start = new RoverState(0, 0, Direction.N);
+        MarsRover marsRover = new MarsRover(start);
+        RoverState end = marsRover.execute(Arrays.asList(new TurnLeftCmd(), new MoveCmd()));
+        assertEquals(new RoverState(-1, 0, Direction.W), end);
+    }
+
+    @Test
+    void should_direction_is_E_and_x_increase_1_when_mars_rover_execute_turn_right_and_move_cmd_given_direction_is_N() {
+        RoverState start = new RoverState(0, 0, Direction.N);
+        MarsRover marsRover = new MarsRover(start);
+        RoverState end = marsRover.execute(Arrays.asList(new TurnRightCmd(), new MoveCmd()));
+        assertEquals(new RoverState(1, 0, Direction.E), end);
     }
 }
